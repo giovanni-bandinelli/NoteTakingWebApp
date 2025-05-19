162c86258ab2648238a582c119edc7eae8a526e2
giovanni <giovabandi02@gmail.com>
2025-05-19 21:06:19 +0200

    .

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://note-taking-web-74qtbivgt-giovannis-projects-5124523b.vercel.app',
+    origin: 'note-taking-web-app-eight-green.vercel.app',
 }));
 
 app.use(express.json());
